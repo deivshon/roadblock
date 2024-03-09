d9fe78b2ceba5473ada5faa615c9d5d18678e538
Davide Cioni <88600810+deivshon@users.noreply.github.com>
2024-03-09 17:49:47 +0100

    feat(config): :sparkles: recursively search for config files

--- cmd/main.go
+++ cmd/main.go
@@ -2,33 +2,61 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"main/pkg/config"
 	"main/pkg/utils"
+	"os"
+	"path/filepath"
 
 	"github.com/mattn/go-shellwords"
 )
 
+const configsName = "config.yml"
+const roadblockConfigDir = "roadblock"
+
 func main() {
 	command := flag.String("t", "", "Target command to evaluate")
+	rootConfigDir := flag.String("c", "", "Configurations root directory")
+	showBrokenRuleConfig := flag.Bool("s", false, "When a command is forbidden, show the configuration path containing the broken rule")
 
 	flag.Parse()
 
-	runtimeConfig, err := config.GetConfig("./examples/config.yaml")
-	if err != nil {
-		utils.Log.Fatalf("could not get config: %v", err)
+	if *rootConfigDir == "" {
+		configDir, err := os.UserConfigDir()
+		if err != nil {
+			utils.Log.Fatalf("could not get user configuration directory: %v", err)
+		}
+
+		*rootConfigDir = filepath.Join(configDir, roadblockConfigDir)
 	}
 
-	parsedCommand, err := shellwords.Parse(*command)
+	configPaths, err := utils.SearchFilesRecursive(*rootConfigDir, configsName)
 	if err != nil {
-		utils.Log.Fatalf("could not parse command into shellwords: %v", err)
+		utils.Log.Fatalf("could not recursively search configurations root directory: %v", err)
 	}
 
-	forbidden, err := runtimeConfig.IsCommandForbidden(parsedCommand)
-	if err != nil {
-		utils.Log.Fatalf("could not evaluate command: %v", err)
-	}
+	for _, p := range configPaths {
+		runtimeConfig, err := config.GetConfig(p)
+		if err != nil {
+			utils.Log.Fatalf("could not get config: %v", err)
+		}
 
-	if forbidden.Forbidden {
-		utils.Log.Fatalf("forbidden by rule `%v`", forbidden.BrokenRule)
+		parsedCommand, err := shellwords.Parse(*command)
+		if err != nil {
+			utils.Log.Fatalf("could not parse command into shellwords: %v", err)
+		}
+
+		forbidden, err := runtimeConfig.IsCommandForbidden(parsedCommand)
+		if err != nil {
+			utils.Log.Fatalf("could not evaluate command: %v", err)
+		}
+
+		if forbidden.Forbidden {
+			configErrorMessage := ""
+			if *showBrokenRuleConfig {
+				configErrorMessage = fmt.Sprintf("\nsource config: %v", p)
+			}
+			utils.Log.Fatalf("forbidden by rule `%v`%s", forbidden.BrokenRule, configErrorMessage)
+		}
 	}
 }
---
+++ pkg/utils/search.go
@@ -0,0 +1,39 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func SearchFilesRecursive(dirPath string, needleFilename string) ([]string, error) {
+	pathData, err := os.Stat(dirPath)
+	if err != nil {
+		return nil, fmt.Errorf("could not stat `%v`: %w", dirPath, err)
+	}
+	if !pathData.IsDir() {
+		return nil, fmt.Errorf("recursive search was passed a non-directory: %v", dirPath)
+	}
+
+	entries, err := os.ReadDir(dirPath)
+	if err != nil {
+		return nil, fmt.Errorf("could not list files in dir: %v", dirPath)
+	}
+
+	results := make([]string, 0)
+	for _, e := range entries {
+		completeEntryPath := filepath.Join(dirPath, e.Name())
+		if !e.IsDir() && e.Name() == needleFilename {
+			results = append(results, completeEntryPath)
+		} else if e.IsDir() {
+			recursiveResults, err := SearchFilesRecursive(completeEntryPath, needleFilename)
+			if err != nil {
+				return nil, err
+			}
+
+			results = append(results, recursiveResults...)
+		}
+	}
+
+	return results, nil
+}
